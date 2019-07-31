5c2bd271a7d7c5b4f3506b52687653350558e8df
Caio Santos <caiosant10@gmail.com>
2019-07-31 15:21:53 -0300

    Rest testado

---
+++ src/main/java/com/projeto/controleDeEstoque/resources/ProdutoResource.java
@@ -0,0 +1,16 @@
+package com.projeto.controleDeEstoque.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/produtos")
+public class ProdutoResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar(){
+		return "REST esta funcionando";
+	}
+
+}
