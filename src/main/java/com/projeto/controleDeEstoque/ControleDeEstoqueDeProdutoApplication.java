6b5732effbec1a7b0a29d184d3c83a05856e1e98
Caio Santos <caiosant10@gmail.com>
2019-07-31 14:56:21 -0300

    Projeto criado

---
+++ src/main/java/com/projeto/controleDeEstoque/ControleDeEstoqueDeProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.controleDeEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeEstoqueDeProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeEstoqueDeProdutoApplication.class, args);
+	}
+
+}
