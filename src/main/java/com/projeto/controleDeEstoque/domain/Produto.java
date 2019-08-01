e7757e540d1fca0d5a81adb20ebb892be34fa29a
Caio Santos <caiosant10@gmail.com>
2019-08-01 09:57:38 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/projeto/controleDeEstoque/domain/Produto.java
+++ src/main/java/com/projeto/controleDeEstoque/domain/Produto.java
@@ -4,11 +4,19 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	private String descricao;
 	private double preco;
